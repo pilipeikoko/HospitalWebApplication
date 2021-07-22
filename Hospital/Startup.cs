0024aa2c7db574777a35e2c705bd6815335a32f0
pilipeikoko <pilipeikoko@gmail.com>
2021-07-22 11:04:25 +0300

    added patients controller

--- Hospital/Startup.cs
+++ Hospital/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hospital.Data;
 
 namespace Hospital
 {
@@ -24,6 +26,9 @@ namespace Hospital
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<HospitalContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("HospitalContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +55,7 @@ namespace Hospital
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Patients}/{action=Index}/{id?}");
             });
         }
     }
