7d87e1018d23a30459e31a720c404a52dffd208c
pilipeikoko <pilipeikoko@gmail.com>
2021-07-22 10:13:18 +0300

    Add project files.

---
+++ Hospital/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Models
+{
+    public class Patient
+    {
+        public string FullName { get; set; }
+        public string PlaceOfResidence { get; set; }
+        public DateTime Birthday { get; set; }
+        
+        public DateTime DateOfReceipt { get; set; }
+        public string DoctrosFullName { get; set; }
+        public string Colnclusion { get; set; }
+
+
+    }
+}
