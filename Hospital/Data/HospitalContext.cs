76cec569e56659ff4adb72578d9bcea237f6c534
pilipeikoko <pilipeikoko@gmail.com>
2021-07-22 11:03:48 +0300

    added database

---
+++ Hospital/Data/HospitalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hospital.Models;
+
+namespace Hospital.Data
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext (DbContextOptions<HospitalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hospital.Models.Patient> Patient { get; set; }
+    }
+}
---
+++ Hospital/Migrations/20210722072103_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hospital.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PlaceOfResidence = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateOfReceipt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DoctrosFullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Colnclusion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patient", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patient");
+        }
+    }
+}
