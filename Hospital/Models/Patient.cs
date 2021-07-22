2666717f9e6d63e7bbb4958bdf1d36891b323aca
pilipeikoko <pilipeikoko@gmail.com>
2021-07-22 11:03:35 +0300

    added validation

--- Hospital/Models/Patient.cs
+++ Hospital/Models/Patient.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Hospital.Validator;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +10,33 @@ namespace Hospital.Models
 {
     public class Patient
     {
+        public int Id { get; set; }
+
+        [Display(Name = "Full name")]
+        [RegularExpression(@"^[a-zA-Z]([-']?[a-z]+)*( [a-zA-Z]([-']?[a-z]+)*)+$")]
+        [Required]
         public string FullName { get; set; }
+
+        [StringLength(60, MinimumLength = 5)]
+        [Display(Name = "Place of residence")]
         public string PlaceOfResidence { get; set; }
+
+        [DataType(DataType.Date)]
+        [DateLessThanOrEqualToToday]
+        [Display(Name = "Birthday")]
         public DateTime Birthday { get; set; }
-        
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Receipt date")]
+        [DateLessThanOrEqualToToday]
         public DateTime DateOfReceipt { get; set; }
+
+        [Display(Name = "Doctors full name")]
+        [RegularExpression(@"^[a-zA-Z]([-']?[a-z]+)*( [a-zA-Z]([-']?[a-z]+)*)+$")]
+        [Required]
         public string DoctrosFullName { get; set; }
+
+        [Display(Name = "Conclusion")]
         public string Colnclusion { get; set; }
 
 
---
+++ Hospital/Validator/DateLessThanOrEqualToToday.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hospital.Validator
+{
+    public sealed class DateLessThanOrEqualToToday : ValidationAttribute
+    {
+        public override string FormatErrorMessage(string name)
+        {
+            return "Date value should not be a future date";
+        }
+
+        protected override ValidationResult IsValid(object objValue,
+                                                       ValidationContext validationContext)
+        {
+            var dateValue = objValue as DateTime? ?? new DateTime();
+
+
+            if (dateValue.Date > DateTime.Now.Date)
+            {
+                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
